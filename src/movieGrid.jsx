d77877c110085655efb0832df6f440239bd5443d
Daiana89 <daianachaves_89@hotmail.com>
2023-07-04 17:55:11 -0300

    Agego texto

--- src/movieGrid.jsx
+++ src/movieGrid.jsx
@@ -4,7 +4,7 @@ import styles from "./movieGrid.module.css";
 
 export function MoviesGrid() {
   return (
-    <><h1>Hello Word!!</h1><ul className={styles.movieGrid}>
+    <><h1>Hello Wooooord!!</h1><ul className={styles.movieGrid}>
       {movies.map((movie) => (<MovieCard key={movie.id} movie={movie}></MovieCard>))}
     </ul></>
   );
