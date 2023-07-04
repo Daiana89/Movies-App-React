4f1104d8943f3fdbbfa302ab463bb180c3e746aa
Daiana89 <daianachaves_89@hotmail.com>
2023-07-04 17:43:43 -0300

    fix: descripción del trabajo

--- src/movieGrid.jsx
+++ src/movieGrid.jsx
@@ -4,8 +4,8 @@ import styles from "./movieGrid.module.css";
 
 export function MoviesGrid() {
   return (
-    <ul className={styles.movieGrid}>
+    <><h1>Hello Word!!</h1><ul className={styles.movieGrid}>
       {movies.map((movie) => (<MovieCard key={movie.id} movie={movie}></MovieCard>))}
-    </ul>
+    </ul></>
   );
 }
